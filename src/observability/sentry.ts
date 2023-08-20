391cab55cfaf0371e22a554ce5e942b2d46d48fd
Suguru Inatomi <suguru.inatomi@gmail.com>
2023-08-20 16:58:18 +0900

    fix: add sentry tranaction integration

--- src/observability/sentry.ts
+++ src/observability/sentry.ts
@@ -1,9 +1,10 @@
-import { Toucan } from 'toucan-js';
+import { Toucan, Transaction } from 'toucan-js';
 
 export function initSentry(dsn: string, context: ExecutionContext) {
   const sentry = new Toucan({
     dsn,
     context,
+    integrations: [new Transaction()],
   });
   return sentry;
 }
